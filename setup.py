a691a17b76bf782be1650a67a8fcd7df5e938b61
Maik Röder <maikroeder@gmail.com>
2009-10-27 16:05:16 +0000

    =Better classifiers and pylinted
    git-svn-id: http://svn.plone.org/svn/collective/groupspace.mail/trunk@100405 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -1,40 +1,57 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+"""
+groupspace.mail
+"""
 import os
 
-version = '1.0'
+from setuptools import find_packages
+from setuptools import setup
+
+__version__  = '1.0'
+
+__here__ = os.path.abspath(os.path.dirname(__file__))
+
+README = open(os.path.join(__here__, 'README.txt')).read()
+CHANGES = open(os.path.join(__here__, 'CHANGES.txt')).read()
 
 setup(name='groupspace.mail',
-      version=version,
-      description="Mail service for the GroupSpace content type of GrufSpaces",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+    version=__version__ ,
+    description="Mail service for the GroupSpace content type of GrufSpaces",
+    long_description=README + '\n\n' +  CHANGES,
+    classifiers=[
+      "Development Status :: 4 - Beta",
+      "Environment :: Web Environment",
+      "Framework :: Plone",
+      "Framework :: Zope2",
+      "License :: OSI Approved :: GNU General Public License (GPL)",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Topic :: Communications :: Email",
+      "Topic :: Internet :: WWW/HTTP :: Dynamic Content",        
+      "Topic :: Office/Business :: Groupware",
+      "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='Maik Roeder',
-      author_email='roeder@berg.net',
-      url='http://svn.plone.org/svn/collective/groupspace.mail',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['groupspace'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
+    keywords='',
+    author='Maik Roeder',
+    author_email='roeder@berg.net',
+    url='http://svn.plone.org/svn/collective/groupspace.mail',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['groupspace'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [distutils.setup_keywords]
-      paster_plugins = setuptools.dist:assert_string_list
+    [distutils.setup_keywords]
+    paster_plugins = setuptools.dist:assert_string_list
 
-      [egg_info.writers]
-      paster_plugins.txt = setuptools.command.egg_info:write_arg
-      """,
-      paster_plugins = ["ZopeSkel"],
-      )
+    [egg_info.writers]
+    paster_plugins.txt = setuptools.command.egg_info:write_arg
+    """,
+    paster_plugins = ["ZopeSkel"],
+    )
